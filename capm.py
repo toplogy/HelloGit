1c02226717d1f432f487d08db0c1f69432f3f3d8
toplogy <50535270+toplogy@users.noreply.github.com>
2025-12-12 20:45:04 +0800

    Add files via upload

---
+++ capm.py
@@ -0,0 +1,13 @@
+import math
+def getReturn(rm,beita):
+    return rm*beita
+def getCov(bx,qx,by,qy,qm):
+    qxy=bx*by*qm*qm/(qx*qy)
+    wx=math.sqrt(qx*qx-bx*bx*qm*qm)
+    wy=math.sqrt(qy*qy-by*by*qm*qm)
+    return(qxy,wx,wy)
+
+if __name__=="__main__":
+    print(getReturn(-0.05,1.05))
+    print(getReturn(0.07,1.05))
+    print(getCov(1.15,0.35,0.95,0.33,0.2))
